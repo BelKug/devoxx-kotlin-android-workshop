eb9cc44458438a4e89d8e2b4cad3e061138f7850
David González <malmstein@gmail.com>
2017-11-01 17:05:36 +0100

    updating color scheme and minSDK

--- app/src/main/java/com/malmstein/samples/beers/BeersActivity.kt
+++ app/src/main/java/com/malmstein/samples/beers/BeersActivity.kt
@@ -8,5 +8,6 @@ class BeersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beers)
+
     }
 }
