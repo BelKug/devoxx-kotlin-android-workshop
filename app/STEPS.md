3198b897ac0932fe80523d08905ce3a6127fef9d
David González <malmstein@gmail.com>
2017-11-03 22:06:33 +0100

    added network dependencies and Retrofit

--- app/STEPS.md
+++ app/STEPS.md
@@ -8,9 +8,7 @@
 8. Infix functions for testing
 9. Invoking functions (GetBeers use case from presenter)
 10. Property delegation
-11. Operator overload
-    - ViewGroup
-12. Coroutines
+11. Coroutines
 
 let / apply / with
 show method().also()
---
+++ app/src/main/java/com/malmstein/samples/beers/BeersApiService.kt
@@ -0,0 +1,32 @@
+package com.malmstein.samples.beers
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface BeersApiService {
+
+    @GET("beers")
+    fun beers(): Call<List<Beer>>
+
+    @GET("beers/{beerId}")
+    fun beerById(@Path("beerId") beerId: Int): Call<Beer>
+
+    companion object Factory {
+
+        val BASE_URL = "http://localhost:8080/api/"
+
+        fun create(): BeersApiService {
+
+            val retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .build()
+
+            return retrofit.create(BeersApiService::class.java);
+        }
+    }
+
+}
